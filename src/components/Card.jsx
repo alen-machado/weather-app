0c6ff388f482ef6928da861f6aa521931983221e
alen-machado <alen.machado@outlook.com>
2021-10-23 01:47:46 -0300

    create card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import CardTemp from './CardTemp';
 export default function Card({max, min, name, img, onClose}) { //ponemos un destructurin y le ponemos las propiedades
   // acá va tu código
   
@@ -8,14 +8,12 @@ export default function Card({max, min, name, img, onClose}) { //ponemos un dest
 <button onClick={onClose}> X </button>  
 <span>{name}</span> 
 <div>
-  <label>Min</label>
-  <span>{min}</span>
-  </div>
-  <div>
-    <label>Max</label>
-    <span>{max}</span>
+ <CardTemp label='Min' value={min}/>
+ <CardTemp label='Max' value={max}/>
 </div>
 <img src={"http://openweathermap.org/img/wn/02d@2x.png"} alt="icono del clima" /> 
   </div>
   )
+
+ 
 };  //es importante respetar el paso a paso antes de comenzar
